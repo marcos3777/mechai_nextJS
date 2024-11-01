f999323a7e0d9beab51458c8feb463091e1c3579
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-10-31 22:57:57 -0300

    refaturado menu e cabecalho com props

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,26 @@
+// componente/Cabecalho.tsx
+
+'use client';
+
 import Menu from "../Menu/Menu";
+import { usePathname } from "next/navigation";
 
 export default function Cabecalho() {
+    const pathname = usePathname();
+
+    // Define a página atual com base na rota
+    let currentPage: "login" | "inicio-meca" | "historico";
+    if (pathname === "/inicio-meca/historico") {
+        currentPage = "historico";
+    } else if (pathname === "/inicio-meca") {
+        currentPage = "inicio-meca";
+    } else {
+        currentPage = "login";
+    }
+
     return (
         <header className="cabecalho">
-            <Menu />
+            <Menu currentPage={currentPage} />
         </header>
-    )
+    );
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,45 @@
+// componente/Menu.tsx
+
 'use client';
 
 import Image from "next/image";
-import logofiap from "@/public/fiap.png"
+import logofiap from "@/public/fiap.png";
+import Link from "next/link";
 
-export default function Menu() {
+interface MenuProps {
+    currentPage: "login" | "inicio-meca" | "historico";
+}
+
+export default function Menu({ currentPage }: MenuProps) {
     const handleReload = () => {
         window.location.href = "/";
     };
+
     return (
-            <div onClick={handleReload}>
-                <Image src={logofiap} alt="Logo da Fiap" className="imgfiap" />
+        <div className="menu flex items-center p-4 ">
+            <div onClick={handleReload} className="cursor-pointer">
+                <Image src={logofiap} alt="Logo da Fiap" className="imgfiap w-16 h-16" />
             </div>
-    )
+            <nav className="nav-menu flex space-x-4 ml-4">
+                {currentPage === "login" && (
+                    <Link href="/sobre" className="text-white hover:underline">
+                        Sobre
+                    </Link>
+                )}
+                {currentPage === "inicio-meca" && (
+                    <>
+
+                        <Link href="/inicio-meca/historico" className="text-white hover:underline">
+                            Histórico
+                        </Link>
+                    </>
+                )}
+                {currentPage === "historico" && (
+                    <Link href="/inicio-meca" className="text-white hover:underline">
+                        Pedidos Finalizados
+                    </Link>
+                )}
+            </nav>
+        </div>
+    );
 }
